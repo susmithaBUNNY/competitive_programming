f55a6b4b6314ba3648bed6e4d2d0bb4f49bd647f
Susmitha <31206125+susmithaBUNNY@users.noreply.github.com>
2018-06-26 17:11:50 +0530

    Create AppleStocks.java

---
+++ week1/day1/AppleStocks.java
@@ -0,0 +1,106 @@
+# competitive_programming
+  import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+
+    public static int getMaxProfit(int[] stockPrices) {
+
+
+    if (stockPrices.length < 2) {
+        throw new IllegalArgumentException("Getting a profit requires at least 2 prices");
+    }
+
+    int minPrice = stockPrices[0];
+    int maxProfit = stockPrices[1] - stockPrices[0];
+    
+    int i = 1;
+    while ( i < stockPrices.length) {
+        int currentPrice = stockPrices[i];
+        int potentialProfit = currentPrice - minPrice;
+        maxProfit = Math.max(maxProfit, potentialProfit);
+        minPrice = Math.min(minPrice, currentPrice);
+        i++;
+    }
+    return maxProfit;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void priceGoesUpThenDownTest() {
+        final int actual = getMaxProfit(new int[] {1, 5, 3, 2});
+        final int expected = 4;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceGoesDownThenUpTest() {
+        final int actual = getMaxProfit(new int[] {7, 2, 8, 9});
+        final int expected = 7;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceGoesUpAllDayTest() {
+        final int actual = getMaxProfit(new int[] {1, 6, 7, 9});
+        final int expected = 8;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceGoesDownAllDayTest() {
+        final int actual = getMaxProfit(new int[] {9, 7, 4, 1});
+        final int expected = -2;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceStaysTheSameAllDayTest() {
+        final int actual = getMaxProfit(new int[] {1, 1, 1, 1});
+        final int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithOnePriceTest() {
+        getMaxProfit(new int[] {5});
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithEmptyPricesTest() {
+        getMaxProfit(new int[] {});
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}

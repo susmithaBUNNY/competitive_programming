08265dde7ce8e80a22b14a48dbda90bc093551bf
Susmitha <31206125+susmithaBUNNY@users.noreply.github.com>
2018-06-28 14:05:11 +0530

    Create mergingtimes.java

---
+++ week1/day1/mergingtimes.java
@@ -0,0 +1,52 @@
+import unittest
+def merge_ranges(time):
+    sortedtime = sorted(time)
+    merge  = [sortedtime[0]]
+    for pres_meetstart, pres_meetend in sortedtime[1:]:
+        past_meetstart, past_meetend = merge[-1]
+        if(pres_meetstart <= past_meetend):
+            merge[-1] = (past_meetstart,max(past_meetend,pres_meetend))
+        else:
+            merge.append((pres_meetstart,pres_meetend))
+    return merge
+
+class Test(unittest.TestCase):
+
+    def test_meetings_overlap(self):
+        actual = merge_ranges([(1, 3), (2, 4)])
+        expected = [(1, 4)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_touch(self):
+        actual = merge_ranges([(5, 6), (6, 8)])
+        expected = [(5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meeting_contains_other_meeting(self):
+        actual = merge_ranges([(1, 8), (2, 5)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_stay_separate(self):
+        actual = merge_ranges([(1, 3), (4, 8)])
+        expected = [(1, 3), (4, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_multiple_merged_meetings(self):
+        actual = merge_ranges([(1, 4), (2, 5), (5, 8)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_not_sorted(self):
+        actual = merge_ranges([(5, 8), (1, 4), (6, 8)])
+        expected = [(1, 4), (5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_sample_input(self):
+        actual = merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+        expected = [(0, 1), (3, 8), (9, 12)]
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
+            

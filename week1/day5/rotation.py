99eb7ff52ebf5fc95b23571c196e387b02e96842
Susmitha <31206125+susmithaBUNNY@users.noreply.github.com>
2018-06-29 10:23:09 +0530

    Create rotation.py

---
+++ week1/day5/rotation.py
@@ -0,0 +1,56 @@
+import unittest
+
+
+def find_rotation_point(words):
+
+    # Find the rotation point in the list
+    for i in range(0,len(words)-1):
+        if(words[i]>words[i+1]):
+            return i+1
+
+    # return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_small_list(self):
+        actual = find_rotation_point(['cape', 'cake'])
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_medium_list(self):
+        actual = find_rotation_point(['grape', 'orange', 'plum',
+                                      'radish', 'apple'])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_large_list(self):
+        actual = find_rotation_point(['ptolemaic', 'retrograde', 'supplant',
+                                      'undulate', 'xenoepist', 'asymptote',
+                                      'babka', 'banoffee', 'engender',
+                                      'karpatka', 'othellolagkage'])
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    # Are we missing any edge cases?
+
+
+unittest.main(verbosity=2)

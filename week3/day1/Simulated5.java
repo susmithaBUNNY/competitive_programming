3f736a4e815c0c3bdf42754d78b069f1dc4829c1
Susmitha <31206125+susmithaBUNNY@users.noreply.github.com>
2018-07-09 17:06:42 +0530

    Create Simulated5.java

---
+++ week3/day1/Simulated5.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+class Solution {
+
+    private static final Random rnd = new Random();
+
+    private static int rand7() {
+        return rnd.nextInt(7) + 1;
+    }
+
+    public static int rand5() {
+    int res = 7;  
+    while (res > 5) {
+        res = res();
+    }
+    return res;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%d ", rand5());
+        }
+        System.out.println();
+    }
+}

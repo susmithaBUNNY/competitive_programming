54b46dc9e9b806df506e49e7e3dd9fc53b7dc939
Susmitha <31206125+susmithaBUNNY@users.noreply.github.com>
2018-07-15 19:39:31 +0530

    Create mergesort..java

---
+++ week3/day4/mergesort..java
@@ -0,0 +1,105 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+
+    public static int[] mergeArrays(int[] myArray, int[] alicesArray) {
+
+        // combine the sorted arrays into one large sorted array
+        
+    int[] answer = new int[myArray.length + alicesArray.length];
+    int i = 0, j = 0, k = 0;
+    while (i < myArray.length && j < alicesArray.length)
+    {
+        if (myArray[i] <alicesArray[j])
+        {
+            answer[k] = myArray[i];
+            i++;
+        }
+        else
+        {
+            answer[k] = alicesArray[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < myArray.length)
+    {
+        answer[k] = myArray[i];
+        i++;
+        k++;
+    }
+
+    while (j < alicesArray.length)
+    {
+        answer[k] = alicesArray[j];
+        j++;
+        k++;
+    }
+
+    return answer;
+    }
+
+
+    // tests
+
+    @Test
+    public void bothArraysAreEmptyTest() {
+        final int[] myArray = {};
+        final int[] alicesArray = {};
+        final int[] expected = {};
+        final int[] actual = mergeArrays(myArray, alicesArray);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void firstArrayIsEmptyTest() {
+        final int[] myArray = {};
+        final int[] alicesArray = {1, 2, 3};
+        final int[] expected = {1, 2, 3};
+        final int[] actual = mergeArrays(myArray, alicesArray);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void secondArrayIsEmptyTest() {
+        final int[] myArray = {5, 6, 7};
+        final int[] alicesArray = {};
+        final int[] expected = {5, 6, 7};
+        final int[] actual = mergeArrays(myArray, alicesArray);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void bothArraysHaveSomeNumbersTest() {
+        final int[] myArray = {2, 4, 6};
+        final int[] alicesArray = {1, 3, 7};
+        final int[] expected = {1, 2, 3, 4, 6, 7};
+        final int[] actual = mergeArrays(myArray, alicesArray);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void arraysAreDifferentLengthsTest() {
+        final int[] myArray = {2, 4, 6, 8};
+        final int[] alicesArray = {1, 7};
+        final int[] expected = {1, 2, 4, 6, 7, 8};
+        final int[] actual = mergeArrays(myArray, alicesArray);
+        assertArrayEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
